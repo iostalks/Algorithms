4afb0123dfc918a0a75a9a512a4271101e0347b7
Smallfly <iostalkboy@gmail.com>
2019-09-08 23:30:36 +0800

    reverse k group with iteration

--- LinkedList/05.ReverseNodesKGroup/ReverseNodesKGroup.js
+++ LinkedList/05.ReverseNodesKGroup/ReverseNodesKGroup.js
@@ -29,3 +29,49 @@ var reverseKGroup = function(head, k) {
 	return head;
 };
 
+/**
+ * 迭代解法
+ * 遍历链表，如果有 k 个，就翻转，然后将翻转后的 group 与已翻转和未翻转的链表合并
+ */
+
+ // Runtime: 68 ms, faster than 92.95% of JavaScript online submissions for Reverse Nodes in k-Group.
+var reverseKGroup2 = function(head, k) {
+	// 初始化一个节点用于指向头结点
+	var dump = jump = new ListNode(0);
+	dump.next = head;
+
+	// l 前一个 group 的 head
+	// r 后一个 group 的 head
+	var l = r = head;
+
+	while (true) {
+		var count = 0;
+		// r 移到下一组 group 的头部
+		while (r && count < k) {
+			r = r.next;
+			count++;
+		}
+		// 翻转 [l, r) 之间的链表
+		if (count === k) {
+			let cur = l, pre = r;
+			while(count--) {
+				let tmp = cur.next;
+				cur.next = pre;
+				pre = cur;
+				cur = tmp;
+			}
+			// 翻转后 pre 为当前 group 头节点
+			// jump 为前一个 group 的尾结点或辅助节点
+			// 将前一个 group 的尾结点指向当前 group 的节点
+			jump.next = pre;
+			// jump 指向当前 group 的最后一个节点
+			jump = l;
+			// l 指向下一组 group 的头节点
+			l = r;
+		} else {
+			// 因为 dump 等于 jump 的初始
+			// 而 jump 的初值的 next 指向链表的头部
+			return dump.next;
+		}
+	}
+};
